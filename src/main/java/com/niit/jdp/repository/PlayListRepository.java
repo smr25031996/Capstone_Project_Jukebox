8bef6a5d8ac5a25c28601d67d1644cac3a17c502
Shubham <rajputshubham615@gmail.com>
2022-09-20 17:51:45 +0530

    added the PlayListRepository.java

---
+++ src/main/java/com/niit/jdp/repository/PlayListRepository.java
@@ -0,0 +1,9 @@
+/*
+ * Author Name: Shubham Rajput
+ * Date: 20-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp.repository;
+
+public class PlayListRepository {
+}
