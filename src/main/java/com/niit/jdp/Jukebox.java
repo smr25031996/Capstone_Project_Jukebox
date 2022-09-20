9b08bdbf8fa3ddb99f44db7633825dd7761c2ed8
Shubham <rajputshubham615@gmail.com>
2022-09-20 17:49:16 +0530

    created the Jukebox.java as main class

---
+++ src/main/java/com/niit/jdp/Jukebox.java
@@ -0,0 +1,12 @@
+/*
+ * Author Name: Shubham Rajput
+ * Date: 20-09-2022
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.niit.jdp;
+
+public class Jukebox {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/niit/jdp/Main.java
+++
@@ -1,7 +0,0 @@
-package com.niit.jdp;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("service world!");
-    }
-}
