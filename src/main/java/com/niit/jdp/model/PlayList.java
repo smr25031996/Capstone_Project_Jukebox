765546433901615975070a15d0ce8b0ffa5da933
Shubham <rajputshubham615@gmail.com>
2022-09-21 10:54:57 +0530

    added the  no_args and parametrised constructors

--- src/main/java/com/niit/jdp/model/PlayList.java
+++ src/main/java/com/niit/jdp/model/PlayList.java
@@ -5,6 +5,18 @@
  */
 package com.niit.jdp.model;
 
-public class PlayList {
+public class PlayList extends Song {
+    // fields for class
+    private int playListId;
+    private String playListName;
 
+    // This is a constructor.
+    public PlayList() {
+    }
+
+    public PlayList(int songId, String songName, String songGenre, String songArtist, int songDuration, String songAlbum, int playListId, String playListName) {
+        super(songId, songName, songGenre, songArtist, songDuration, songAlbum);
+        this.playListId = playListId;
+        this.playListName = playListName;
+    }
 }
