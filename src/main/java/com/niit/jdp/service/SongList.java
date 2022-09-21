d9a958f222a9c81240ef98c905c1f9bbd14beefa
Shubham <rajputshubham615@gmail.com>
2022-09-21 12:59:30 +0530

    added the logic to print the song names

--- src/main/java/com/niit/jdp/service/SongList.java
+++ src/main/java/com/niit/jdp/service/SongList.java
@@ -5,5 +5,23 @@
  */
 package com.niit.jdp.service;
 
+import com.niit.jdp.model.Song;
+import com.niit.jdp.repository.SongRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class SongList {
+    // Creating an object of SongRepository class.
+    SongRepository songRepository = new SongRepository();
+    DatabaseService databaseService = new DatabaseService();
+
+    // Creating an object of DatabaseService class.
+    public void displaySongMenu() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        List<Song> allSong = songRepository.getAllSong(databaseService.getDatabaseConnection());
+        System.out.println(allSong);
+
+
+    }
 }
