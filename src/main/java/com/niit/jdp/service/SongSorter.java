809d18f4f09b0e2d7d361748558c344f8deda63c
Shubham <rajputshubham615@gmail.com>
2022-09-21 17:38:17 +0530

    created the class

--- src/main/java/com/niit/jdp/service/SongSorter.java
+++ src/main/java/com/niit/jdp/service/SongSorter.java
@@ -3,9 +3,8 @@
  * Date: 21-09-2022
  * Created With: IntelliJ IDEA Community Edition
  */
-package com.niit.jdp.repository;
+package com.niit.jdp.service;
 
-public class SongSort extends SongRepository {
-
-
+public class SongSorter {
+    
 }
