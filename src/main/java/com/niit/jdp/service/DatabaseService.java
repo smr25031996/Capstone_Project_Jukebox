b89bee4bee260cdc6c9afc9ea4a63afd3192acdf
Shubham <rajputshubham615@gmail.com>
2022-09-21 10:41:48 +0530

    added the url,username,password  to establish the connection

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -5,6 +5,18 @@
  */
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+
+    // the URL of the database
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
+
+    // the credentials of the user trying to log in to the database
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Rshubham@13";
+
+    // a private field to store the reference of the connection object
+    private Connection databaseConnection;
 
 }
