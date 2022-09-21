69f9e660407f6cb6f0f3db0b0c508a49d86e1164
Shubham <rajputshubham615@gmail.com>
2022-09-21 17:36:51 +0530

    moved the class

--- src/main/java/com/niit/jdp/repository/SongSort.java
+++ src/main/java/com/niit/jdp/repository/SongSort.java
@@ -3,7 +3,9 @@
  * Date: 21-09-2022
  * Created With: IntelliJ IDEA Community Edition
  */
-package com.niit.jdp.service;
+package com.niit.jdp.repository;
 
-public class SongSort {
+public class SongSort extends SongRepository {
+
+
 }
