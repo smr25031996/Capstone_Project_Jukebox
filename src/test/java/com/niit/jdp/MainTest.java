8c426b02743b212e7e653b463979696b6d414eec
Shubham <rajputshubham615@gmail.com>
2022-09-24 22:57:33 +0530

    added the test class for main method

---
+++ src/test/java/com/niit/jdp/MainTest.java
@@ -0,0 +1,20 @@
+package com.niit.jdp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
