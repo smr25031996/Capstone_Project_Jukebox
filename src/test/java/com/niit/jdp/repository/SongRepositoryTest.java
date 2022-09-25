c9b86e869ce1c7a59a0db11acc41c7c0031da34f
Shubham <rajputshubham615@gmail.com>
2022-09-25 15:48:47 +0530

    added the test cases songRepository

--- src/test/java/com/niit/jdp/repository/SongRepositoryTest.java
+++ src/test/java/com/niit/jdp/repository/SongRepositoryTest.java
@@ -1,9 +1,17 @@
 package com.niit.jdp.repository;
 
+import com.niit.jdp.exception.ArtistNotFoundException;
+import com.niit.jdp.model.Song;
 import com.niit.jdp.service.DatabaseService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class SongRepositoryTest {
     SongRepository songRepository;
@@ -22,28 +30,62 @@ class SongRepositoryTest {
     }
 
     @Test
-    void getAll() {
+    void getAll() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        List<Song> actual = songRepository.getAll(databaseService.getDatabaseConnection());
+        assertNotEquals(null, actual);
+    }
 
+    @Test
+    void getSongById() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        String expected = "Yeh_Jo_Mohabbat";
+        String actual = songRepository.getSongById(databaseService.getDatabaseConnection(), 9);
+        assertEquals(expected, actual, "You Entered wrong id");
+        String songName = "Dil_Se_Re";
+        assertNotEquals(songName, songRepository.getSongById(databaseService.getDatabaseConnection(), 8));
+    }
+
+    @Test
+    void getSongBySongName() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        String expected = "Mere_Sapnon";
+        String actual = songRepository.getSongBySongName(databaseService.getDatabaseConnection(), "Agar_Tum_Saath_Ho", "songslist");
+        assertNotEquals(expected, actual, "You Entered wrong id");
+        String songName = "Dil_Se_Re";
+        assertEquals(songName, songRepository.getSongBySongName(databaseService.getDatabaseConnection(), "Dil_Se_Re", "songslist"));
+    }
+
+    @Test
+    void getSongByAlbum() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        String expected = "Mere_Sapnon";
+        String actual = songRepository.getSongByAlbum(databaseService.getDatabaseConnection(), "Aradhana", "songslist");
+        assertEquals(expected, actual, "You Entered wrong id");
+        String songName = "Dil_Se_Re";
+        assertNotEquals(songName, songRepository.getSongByAlbum(databaseService.getDatabaseConnection(), "Tamasha", "songslist"));
 
     }
 
     @Test
-    void getSongById() {
+    void getSongByGenre() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        String expected = "Kabhi_Neem_Neem";
+        String actual = songRepository.getSongByGenre(databaseService.getDatabaseConnection(), "Bollywood", "songslist");
+        assertEquals(expected, actual, "You Entered wrong id");
+        String songName = "Dil_Se_Re";
+        assertNotEquals(songName, songRepository.getSongByGenre(databaseService.getDatabaseConnection(), "Bollywood", "songslist"));
+
     }
 
     @Test
-    void getSongBySongName() {
-    }
+    void getSongByArtists() throws SQLException, ClassNotFoundException, ArtistNotFoundException {
+        databaseService.connect();
+        String expected = "Yeh_Kya_Hua";
+        String actual = songRepository.getSongByArtists(databaseService.getDatabaseConnection(), "Rajesh_Khanna", "songslist");
+        assertEquals(expected, actual, "You Entered wrong id");
+        String songName = "Dil_Se_Re";
+        assertNotEquals(songName, songRepository.getSongByArtists(databaseService.getDatabaseConnection(), "Dil_Se", "songslist"));
 
-    @Test
-    void getSongByAlbum() {
-    }
-
-    @Test
-    void getSongByGenre() {
-    }
-
-    @Test
-    void getSongByArtists() {
     }
 }
