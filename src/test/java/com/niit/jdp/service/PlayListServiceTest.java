5e7771ec5a2c43d825a071324eaac586d8a4ad43
Shubham <rajputshubham615@gmail.com>
2022-09-25 16:47:05 +0530

    written the test cases for methods

--- src/test/java/com/niit/jdp/service/PlayListServiceTest.java
+++ src/test/java/com/niit/jdp/service/PlayListServiceTest.java
@@ -1,24 +1,47 @@
 package com.niit.jdp.service;
 
+import com.niit.jdp.repository.SongRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 class PlayListServiceTest {
+    PlayListService playListService;
+    SongRepository songRepository;
+    DatabaseService databaseService;
 
     @BeforeEach
     void setUp() {
+        playListService = new PlayListService();
+        songRepository = new SongRepository();
+        databaseService = new DatabaseService();
     }
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
-    void sortAccordingToGivenType() {
-        String expected = "";
+    void sortAccordingToGivenType() throws SQLException, ClassNotFoundException {
+        databaseService.connect();
+        // Checking that the result of the method is not null.
+        assertNotEquals(null, playListService.sortAccordingToGivenType("song", songRepository
+                .getAll(databaseService.getDatabaseConnection())));
 
+        // Checking that the result of the method is not null.
+        assertNotEquals(null, playListService.sortAccordingToGivenType("artist", songRepository
+                .getAll(databaseService.getDatabaseConnection())));
+        // Checking that the result of the method is not null.
+        assertNotEquals(null, playListService.sortAccordingToGivenType("genre", songRepository
+                .getAll(databaseService.getDatabaseConnection())));
+        // Checking that the result of the method is not null.
+        assertNotEquals(null, playListService.sortAccordingToGivenType("album", songRepository
+                .getAll(databaseService.getDatabaseConnection())));
     }
-
 
 }
